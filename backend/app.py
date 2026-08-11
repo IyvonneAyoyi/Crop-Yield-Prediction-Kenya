6e67649199281ca29b0b75f04e9781953c53e7ce
Jeptooleonida <jeptooleonida@gmail.com>
2026-08-12 02:06:10 +0300

    update cors

--- backend/app.py
+++ backend/app.py
@@ -30,7 +30,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",  # React (Vite)
+     "https://crop-yield-prediction-kenya.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
