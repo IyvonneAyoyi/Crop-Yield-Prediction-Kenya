97b37fb44b742eb100172003dbd2f064967ff7b8
IyvonneAyoyi <70914411+IyvonneAyoyi@users.noreply.github.com>
2026-08-12 00:18:35 +0300

    Update config.py

--- backend/gee/config.py
+++ backend/gee/config.py
@@ -33,6 +33,7 @@ else:
         project="geospatial-rs"
     )
 
+
 # ==========================================================
 # LOAD COUNTY BOUNDARIES
 # ==========================================================
@@ -41,6 +42,7 @@ COUNTIES = ee.FeatureCollection(
     "projects/geospatial-rs/assets/Kenya_Counties_Shapefiles"
 )
 
+
 # ==========================================================
 # SPAM CROP MASKS
 # ==========================================================
@@ -48,27 +50,26 @@ COUNTIES = ee.FeatureCollection(
 CROP_MASKS = {
 
     "Maize":
-    "projects/geospatial-rs/assets/spam2020_V2r2_global_H_MAIZE",
+        "projects/geospatial-rs/assets/spam2020_V2r2_global_H_MAIZE",
 
     "Beans":
-    "projects/geospatial-rs/assets/spam2020_V2r2_global_H_BEANS",
+        "projects/geospatial-rs/assets/spam2020_V2r2_global_H_BEANS",
 
     "Cowpeas":
-    "projects/geospatial-rs/assets/spam2020_V2r2_global_H_COWPEAS",
+        "projects/geospatial-rs/assets/spam2020_V2r2_global_H_COWPEAS",
 
     "Irish Potatoes":
-    "projects/geospatial-rs/assets/spam2020_V2r2_global_H_IRISHPOTATOES",
+        "projects/geospatial-rs/assets/spam2020_V2r2_global_H_IRISHPOTATOES",
 
     "Pigeon Peas":
-    "projects/geospatial-rs/assets/spam2020_V2r2_global_H_PIGEONPEAS",
+        "projects/geospatial-rs/assets/spam2020_V2r2_global_H_PIGEONPEAS",
 
     "Sorghum":
-    "projects/geospatial-rs/assets/spam2020_V2r2_global_H_SORGHUM",
+        "projects/geospatial-rs/assets/spam2020_V2r2_global_H_SORGHUM",
 
     "Sweet Potatoes":
-    "projects/geospatial-rs/assets/spam2020_V2r2_global_H_SWEETPOTATOES",
+        "projects/geospatial-rs/assets/spam2020_V2r2_global_H_SWEETPOTATOES",
 
     "Wheat":
-    "projects/geospatial-rs/assets/spam2020_V2r2_global_H_WHEAT"
-
+        "projects/geospatial-rs/assets/spam2020_V2r2_global_H_WHEAT"
 }
