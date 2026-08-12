7173dc4ed5730d6a2a929c3808947879a060aa84
Jeptooleonida <jeptooleonida@gmail.com>
2026-08-12 03:05:41 +0300

    update frontend UI

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,8 +3,7 @@ import axios from "axios";
 // FastAPI base URL
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://127.0.0.1:8000",
+    import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
